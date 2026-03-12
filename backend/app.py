4f036315515c0ec261decfe4e111e683c4c13a7b
LongHai <Phamnguyenlonghai240106@gmail.com>
2026-03-12 12:51:40 +0700

    API

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from ai import best_move
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/move", methods=["POST"])
+def move():
+    data = request.json
+    board = data["board"]
+
+    r,c = best_move(board)
+
+    return jsonify({
+        "row" : r,
+        "col" : c 
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
